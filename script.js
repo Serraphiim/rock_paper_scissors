1d2ae7432a245609b54f3cf0d2a2db8c05fd9f1c
Serraphiim <gibschnurr@gmail.com>
2018-07-13 13:09:47 -0500

    completed incrementing functions build. Next step is to end game when 5 score is reached by either side.

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
-
+let userScore = 0;
+let aiScore = 0;
+document.getElementById('userScore').innerHTML = userScore;
+document.getElementById('aiScore').innerHTML = aiScore;
 //the round function is using an arrow function () => I am not entirely sure what it does I should research
-
 function round() {
   rockButton.addEventListener('click', () => getRoundResult('rock'));
   paperButton.addEventListener('click', () => getRoundResult('paper'));
   scissorsButton.addEventListener('click', () => getRoundResult('scissors'));
+}
+
+function getRoundResult(userPick) {
+  let aiPick = getAiPick();
+    switch (userPick + aiPick) {
+      case 'rockpaper':
+      case 'paperscissors':
+      case 'scissorsrock':
+        userLoseRound(userPick, aiPick);
+        break;
+      case 'rockscissors':
+      case 'paperrock':
+      case 'scissorspaper':
+        userWinRound(userPick, aiPick);
+        break;
+      case 'rockrock':
+      case 'paperpaper':
+      case 'scissorsscissors':
+        drawRound(userPick, aiPick);
+        break;
+    }
 }
 
 function getAiPick() {
@@ -23,25 +46,24 @@ function getAiPick() {
 
 }
 
-function getRoundResult(userPick) {
-  let aiPick = getAiPick();
-    switch (userPick + aiPick) {
-      case 'rockpaper':
-      case 'paperscissors':
-      case 'scissorsrock':
-        return document.getElementById('test').innerHTML = aiPick + ' beats ' + userPick + ', you lose!';
-        break;
-      case 'rockscissors':
-      case 'paperrock':
-      case 'scissorspaper':
-        return document.getElementById('test').innerHTML = userPick + ' beats ' + aiPick + ', you win!';
-        break;
-      case 'rockrock':
-      case 'paperpaper':
-      case 'scissorsscissors':
-        return document.getElementById('test').innerHTML = 'It is a draw!';
-        break;
-    }
+function userLoseRound(userPick, aiPick) {
+  aiScore++;
+  document.getElementById('test').innerHTML = aiPick + ' beats ' + userPick+ ', you lose!';
+  document.getElementById('userScore').innerHTML = userScore;
+  document.getElementById('aiScore').innerHTML = aiScore;
+}
+
+function userWinRound(userPick, aiPick) {
+  userScore++;
+  document.getElementById('test').innerHTML = userPick + ' beats ' + aiPick+ ', you win!';
+  document.getElementById('userScore').innerHTML = userScore;
+  document.getElementById('aiScore').innerHTML = aiScore;
+}
+
+function drawRound(userPick, aiPick) {
+  document.getElementById('test').innerHTML = ' Both players chose ' + userPick+ ', it is a draw!';
+  document.getElementById('userScore').innerHTML = userScore;
+  document.getElementById('aiScore').innerHTML = aiScore;
 }
 
 round();
